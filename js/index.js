8cf81d4c184e8d0423998583bfb2da8eef575a82
OOWGNOD <86402288+OOWGNOD@users.noreply.github.com>
2021-10-05 09:50:20 +0900

    12
    12

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(function() {
+	$('#fullpage').fullpage({
+		//options here
+		licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+		autoScrolling:true,
+		scrollHorizontally: true,
+		navigation: true,
+		navigationPosition: 'right',
+		loopBottom: true,
+		loopTop: true
+		,
+	});
+
+});
+
